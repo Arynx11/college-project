dccd0489188cfbb20d6ef065ff6d97efdafe1294
Aryanw <arynxaryan@gmail.com>
2026-02-17 02:40:42 +0530

    final

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const bookingRoutes = require('./routes/bookingRoutes');
 const app = express();
 
 // Middleware
-app.use(cors(config.cors));
+app.use(cors());
 app.use(express.json());
 
 // Routes
